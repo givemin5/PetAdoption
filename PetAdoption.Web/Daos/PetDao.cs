7c589c5acfbabdf4957d45b07891d2df7bcf1c34
givemin5 <givemin5@gmail.com>
2018-04-09 08:21:49 +0800

    取得資料

---
+++ PetAdoption.Web/Daos/PetDao.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json;
+using PetAdoption.Web.Models;
+
+namespace PetAdoption.Web.Daos
+{
+    public class PetDao
+    {
+        public List<PetJsonModel> GetAll()
+        {
+            string url = "http://data.coa.gov.tw/Service/OpenData/AnimalOpenData.aspx";
+
+            HttpWebRequest req = WebRequest.Create(new Uri(url)) as HttpWebRequest;
+            req.Method = "GET";
+
+            WebResponse rs = req.GetResponse();
+
+            using (Stream stream = rs.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                String responseString = reader.ReadToEnd();
+
+                var petJsons = JsonConvert.DeserializeObject<List<PetJsonModel>>(responseString);
+
+                return petJsons;
+            }
+        }
+    }
+}
